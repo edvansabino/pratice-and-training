960fc12dbcbbba1f7cbf12f5a4553349507f77a4
edvansabino <edvansabino@gmail.com>
2022-10-30 19:53:25 -0300

    Esboço final de main

--- cod-diversos/project-piano-keyboard/youtube-masterclass-dom-master/main.js
+++ cod-diversos/project-piano-keyboard/youtube-masterclass-dom-master/main.js
@@ -1,41 +1,60 @@
 // get all keys
-const myKeys = document.querySelectorAll(".keys")
+const myKeys = document.querySelectorAll(".key");
 
 console.log(myKeys);
 
-
 // play notes
-function playNote(event){
+function playNote(event) {
+  let audioKeyCode = getKeyCode(event);
 
-let audioKeyCode = getKeyCode(event);
-
-  
   // typed or pressed key
-const key = document.querySelector(`[data-key="${audioKeyCode}]`)
+  const key = document.querySelector(`.key[data-key="${audioKeyCode}]`);
   // if key exists
-const cantFoundAnyKey = !key
+  const cantFoundAnyKey = !key;
 
-if(cantFoundAnyKey) {
-  return;
+  if (cantFoundAnyKey) {
+    return;
+  }
+
+  addPlayingClass(key)
+  playAudio(audioKeyCode)
 }
-  // play audio
+
+function addPlayingClass(key) {
+  key.classList.add('playing')
 }
 
 function getKeyCode(event) {
   let keyCode;
 
-  const isKeyboard = event.type === "keydown"
-    if (isKeyboard) {
-      keyCode = event.keyCode
-    } else {
-      keyCode = event.target.dataset.key
-    }
-    return keyCode;
+  const isKeyboard = event.type === "keydown";
+  if (isKeyboard) {
+    keyCode = event.keyCode;
+  } else {
+    keyCode = event.target.dataset.key;
   }
+  return keyCode;
+}
 
-// click with mouse
-myKeys.forEach( function(key) {
-  key.addEventListener("click", playNote)
-})
-//keyboard type
-window.addEventListener("keydown", playNote)
+function playAudio(audioKeyCode) {
+  const audio = document.querySelector(`audio[data-key="${audioKeyCode}]`);
+  audio.currentTime = 0;
+  audio.play();
+}
+
+function removePlayingClass(event) {
+  event.target.classList.remove("playing")
+}
+
+function registerEvents() {
+  // click with mouse
+  myKeys.forEach(function (key) {
+    key.addEventListener("click", playNote)
+    key.addEventListener("transitionend", removePlayingClass)
+  
+  });
+  //keyboard type
+  window.addEventListener("keydown", playNote);
+}
+
+window.addEventListener("load", registerEvents);
