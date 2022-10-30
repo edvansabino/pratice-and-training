e7f7af1ea4788c6e420c79632a90ba8cc0e2b9fb
edvansabino <edvansabino@gmail.com>
2022-10-30 19:08:52 -0300

    Buscando e adicionando teclas

--- cod-diversos/project-piano-keyboard/youtube-masterclass-dom-master/main.js
+++ cod-diversos/project-piano-keyboard/youtube-masterclass-dom-master/main.js
@@ -6,10 +6,36 @@ console.log(myKeys);
 
 // play notes
 function playNote(event){
-  console.log(event);
+
+let audioKeyCode = getKeyCode(event);
+
+  
+  // typed or pressed key
+const key = document.querySelector(`[data-key="${audioKeyCode}]`)
+  // if key exists
+const cantFoundAnyKey = !key
+
+if(cantFoundAnyKey) {
+  return;
+}
+  // play audio
 }
 
-// click with mouse
+function getKeyCode(event) {
+  let keyCode;
 
+  const isKeyboard = event.type === "keydown"
+    if (isKeyboard) {
+      keyCode = event.keyCode
+    } else {
+      keyCode = event.target.dataset.key
+    }
+    return keyCode;
+  }
+
+// click with mouse
+myKeys.forEach( function(key) {
+  key.addEventListener("click", playNote)
+})
 //keyboard type
 window.addEventListener("keydown", playNote)
